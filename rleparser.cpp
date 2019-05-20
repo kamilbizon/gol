efaa830e0464c32d973cfb770fe7514f6b0ae0b9
Kamil Bizoń <kamilbizon@outlook.com>
2019-05-20 02:54:53 +0200

    drawing no symetric board not works reading file works

---
+++ rleparser.cpp
@@ -0,0 +1,6 @@
+#include "rleparser.h"
+
+RLEParser::RLEParser()
+{
+
+}
---
+++ rleparser.h
@@ -0,0 +1,11 @@
+#ifndef RLEPARSER_H
+#define RLEPARSER_H
+
+
+class RLEParser
+{
+public:
+    RLEParser();
+};
+
+#endif // RLEPARSER_H
